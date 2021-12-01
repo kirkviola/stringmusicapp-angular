0b1aae8198ba62c1b2509bedad62f1e1ff2eebf7
Matthew Kirkendall <kirk.viola@gmail.com>
2021-12-01 16:09:35 -0500

    Added some basic components and initial images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserlistComponent } from './users/userlist/userlist.component';
+import { UserloginComponent } from './users/userlogin/userlogin.component';
+import { MenuComponent } from './misc/menu/menu.component';
+import { MenuItemComponent } from './misc/menu-item/menu-item.component';
+import { E404Component } from './misc/e404/e404.component';
+import { HomeComponent } from './misc/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserlistComponent,
+    UserloginComponent,
+    MenuComponent,
+    MenuItemComponent,
+    FormsModule,
+    HttpClientModule,
+    E404Component,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
